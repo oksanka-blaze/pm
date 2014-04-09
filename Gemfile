6599d7b6000d18d0012c8148a109300db3b31ce5
Oksana <oksanka.blaze@gmail.com>
2014-04-09 18:20:57 +0300

    bootstrap & nested form

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
 source 'https://rubygems.org'
 
+gem "twitter-bootstrap-rails"
+
 #Authentication
 gem 'devise'
 
--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,8 @@ module Pm
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
+    SimpleForm.browser_validations = false
+
     config.generators do |g|
       g.test_framework :rspec, :view_spec => false,
                        :fixture => true,
